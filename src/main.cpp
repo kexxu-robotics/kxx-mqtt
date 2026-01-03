916e308835df99d4d395ea21790a37e306ecda9d
Jurriaan <jurschreuder@gmail.com>
2026-01-03 15:20:30 +0100

    expanded the example in main.cpp a bit more

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,44 @@
 
+#include <print>
+
 #include <kxx/mqtt/client.hpp>
+#include <kxx/mqtt/format.hpp> // for using std::println and std::format on mqtt settings and status codes
 
 int main(){
 
-  // Publisher
-  kxx::mqtt::MqttClient publisher("publisher_main");
-  if (publisher.connect("localhost", 1883)) {
-    for (int i = 0; i < 5; i++) {
-      std::string msg = "Message #" + std::to_string(i);
-      publisher.publish("kxx_mqtt_test/data", msg, kxx::mqtt::QOS_1);
-      std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  // Assumes you have a basic MQTT setup running on port 1883 without username and password
+
+  // Create Client
+  kxx::mqtt::MqttClient client{"kxx_mqtt_client_example"};
+
+  // handle messages received
+  client.set_message_handler(
+    [](const std::string& topic, const std::vector<uint8_t>& payload, kxx::mqtt::QoS qos, bool retain) {
+      std::string message(std::from_range, payload);
+      std::println(
+          "on_message QOS_0 with topic: {}, QoS: {}, retain: {}, message: {}", 
+          topic, qos, retain, message
+        );
     }
-  }
+  );
+
+	if (client.connect("localhost", 1883)) {
+
+    // subscribe to some test topics
+		client.subscribe("kxx_mqtt_test/qos0", kxx::mqtt::QOS_0);
+		client.subscribe("kxx_mqtt_test/qos1", kxx::mqtt::QOS_1);
+		client.subscribe("kxx_mqtt_test/qos2", kxx::mqtt::QOS_2);
+
+
+    // send some test messages
+		for (int i = 0; i < 5; i++) {
+			std::string msg = std::format("Message #{}", i);
+			client.publish("kxx_mqtt_test/qos0", msg, kxx::mqtt::QOS_0);
+			client.publish("kxx_mqtt_test/qos1", msg, kxx::mqtt::QOS_1);
+			client.publish("kxx_mqtt_test/qos2", msg, kxx::mqtt::QOS_2);
+
+			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		}
+	}
 
 }
